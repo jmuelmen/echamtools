1a7d76ee099ec6da727a1f86aff7e99a9ac3efb5
Johannes Muelmenstaedt <johannes.muelmenstaedt@uni-leipzig.de>
2016-08-21 14:03:33 +0200

    added plyr:: namespace in ldply calls

--- R/get.R
+++ R/get.R
@@ -1,17 +1,17 @@
-get.phase.at.locations.echam <- function(locations) {
-    df <- readRDS("cfsites/echam-warm-frac-tbl.rds") 
-    df %>% group_by(site) %>% summarize(warm.frac = (function(mask) {
-        tbl <- table(mask)
-        tbl["warm"] / (tbl["warm"] + tbl["cold"])
-    })(mask)) %>%
-        rename(id = site) %>% right_join(locations) %>%
-        mutate(center = "MPI-M", model = "MPI-ESM-LR", ens = "r1i1p1") %>%
-        rename(station = name, liquid = warm.frac)
-}
+## get.phase.at.locations.echam <- function(locations) {
+##     df <- readRDS("cfsites/echam-warm-frac-tbl.rds") 
+##     df %>% group_by(site) %>% summarize(warm.frac = (function(mask) {
+##         tbl <- table(mask)
+##         tbl["warm"] / (tbl["warm"] + tbl["cold"])
+##     })(mask)) %>%
+##         rename(id = site) %>% right_join(locations) %>%
+##         mutate(center = "MPI-M", model = "MPI-ESM-LR", ens = "r1i1p1") %>%
+##         rename(station = name, liquid = warm.frac)
+## }
 
 #' @export
 get.pr.echam <- function(ccrauts = c(0, 0.01, 0.1, 1, 2, 3.75, 7.5, 15, 30, 60)) {
-    ldply(ccrauts, function(ccraut) {
+    plyr::ldply(ccrauts, function(ccraut) {
         experiment <- sprintf("amip-rain-%g", ccraut)
         readRDS(sprintf("pr-hist-%s.rds", experiment)) %>%
             cbind(ccraut = ccraut)
@@ -20,7 +20,7 @@ get.pr.echam <- function(ccrauts = c(0, 0.01, 0.1, 1, 2, 3.75, 7.5, 15, 30, 60))
 
 #' @export
 get.rad.echam <- function(ccrauts = c(0, 0.01, 0.1, 1, 2, 3.75, 7.5, 15, 30, 60)) {
-    ldply(ccrauts, function(ccraut) {
+    plyr::ldply(ccrauts, function(ccraut) {
         experiment <- sprintf("amip-rain-%g", ccraut)
         readRDS(sprintf("rad-%s.rds", experiment)) %>%
             cbind(ccraut = ccraut)
@@ -29,7 +29,7 @@ get.rad.echam <- function(ccrauts = c(0, 0.01, 0.1, 1, 2, 3.75, 7.5, 15, 30, 60)
 
 #' @export
 get.mask.echam <- function(ccrauts = c(0, 0.01, 0.1, 1, 2, 3.75, 7.5, 15, 30, 60)) {
-    ldply(ccrauts, function(ccraut) {
+    plyr::ldply(ccrauts, function(ccraut) {
         experiment <- sprintf("amip-rain-%g", ccraut)
         readRDS(sprintf("%s.rds", experiment)) %>%
             cbind(ccraut = ccraut)
