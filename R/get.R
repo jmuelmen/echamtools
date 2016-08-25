141c008990e931a626e2f0108483a6dd826359b4
Johannes Muelmenstaedt <johannes.muelmenstaedt@uni-leipzig.de>
2016-08-25 13:58:30 +0200

    yet more fix

--- R/get.R
+++ R/get.R
@@ -12,16 +12,18 @@
 #' @export
 get.pr.echam <- function(ccrauts = c(0, 0.01, 0.1, 1, 2, 3.75, 7.5, 15, 30, 60),
                          ccaulocs = NA,
+                         flux = TRUE,
                          amip = TRUE) {
     plyr::ddply(expand.grid(ccraut = ccrauts,
                             ccauloc = ccaulocs),
                 ~ ccraut + ccauloc,
                 function(df)
                     with(df, {
-                        experiment <- sprintf("%s%g%s",
+                        experiment <- sprintf("%s%g%s%s",
                                               ifelse(amip, "amip-rain-", "rain_"),
                                               ccraut,
-                                              ifelse(is.na(ccauloc), "", sprintf("_%g", ccauloc)))
+                                              ifelse(is.na(ccauloc), "", sprintf("_%g", ccauloc)),
+                                              ifelse(flux, "", "-mr"))
                         readRDS(sprintf("pr-hist-%s.rds", experiment)) %>%
                             cbind(ccraut = ccraut,
                                   ccauloc = ccauloc)
